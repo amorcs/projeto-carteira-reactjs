3a246499b79cd8f5b02e4f639de8b5a5ec0adcc4
amorcs <marco.sds@hotmail.com>
2021-01-13 20:50:15 -0300

    priemeiro commit do projeto

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,18 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#dcdcdc',
+    secoundary: '#ffffff',
+    tertiary: '#f5f5f5',
+    
+    white: '#000',
+    black: '#fff',
+    gray: '#bfbfbf',
+
+    sucess: '#03bb85',
+    info: '#3b5998',
+    warning: '#ff6961',
+
+  },
+};
